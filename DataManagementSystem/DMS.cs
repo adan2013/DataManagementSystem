90a0a377d0ab254815b35718ec510a8d85b8bd35
adan2013 <adan2013@poczta.fm>
2018-09-12 14:11:18 +0200

    trying to understand c#

--- DataManagementSystem/DMS.cs
+++ DataManagementSystem/DMS.cs
@@ -4,26 +4,60 @@ namespace DataManagementSystem
 {
     public class DMS
     {
-        public DMS()
+        //EVENTS
+        public delegate void FileSavedDelegate(DMS sender, string path);
+        public event FileSavedDelegate FileSaved;
+
+        //READONLY PROPERTIES
+        public WorkingMode Mode { get; private set; }
+        public string PathToFile { get; private set; }
+        public bool FileChanged { get; private set; }
+
+        //ENUMS
+        public enum WorkingMode
         {
-            
+            StaticFile = 0,
+            ProjectFile = 1
+        }
+
+        public DMS(WorkingMode mode, string file)
+        {
+            Mode = mode;
+            PathToFile = file;
+            FileChanged = false;
         }
 
         public static string GetInfo()
         {
-            return "This library was created by adan2013. More info and full documentation you can find here: https://github.com/adan2013";
+            return "This library was created by adan2013. More info and full documentation you can find here: https://github.com/adan2013/DataManagementSystem";
         }
 
         #region "SAVING"
 
-        public void SaveAs()
+        public bool SaveAs(string newpath, bool DeleteExistFile)
         {
             //TODO save as
+            if(System.IO.File.Exists(newpath))
+            {
+                if(DeleteExistFile)
+                {
+                    try
+                    {
+                        System.IO.File.Delete(newpath);
+                    }
+                    catch { return false; }
+                }
+                else { return false; }
+            }
+            PathToFile = newpath;
+            SaveChanges();
+            return true;
         }
 
         public void SaveChanges()
         {
             //TODO save changes
+            FileSaved(this, PathToFile);
         }
 
         #endregion
